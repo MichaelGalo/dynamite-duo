f9993b265649dcecd61ec6bda09423ae5cd56bc3
Michael Galo <michael@michaelthinks.co>
2024-05-24 15:05:15 -0500

    ending first project module

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,26 @@
-const theExistingMainElement = document.querySelector("#container");
+// Import the function references that generate the HTML lists
+import { HeroList } from "./heroes.js";
+import { VillainList } from "./villains.js";
+
+// Capture the return values of the HTML list generators
+const heroListHTML = HeroList();
+const villainListHTML = VillainList();
+
+// Build up a single string containing both chunks of HTML
+const theWholeEnchilada = `
+    <h1>Heroes</h1>
+    ${heroListHTML}
+
+    <h1>Villains</h1>
+    ${villainListHTML}
+`;
 // debugger;
+
+/*
+    This is the code you wrote in the last chapter to get a
+    reference to the <main> element
+*/
+const theExistingMainElement = document.querySelector("#container");
+theExistingMainElement.innerHTML = theWholeEnchilada;
+// .textContent will only display text - this is better for speed & security
+// .innerHTML will also look for html stuff - this is better for when you're actually making stuff**
